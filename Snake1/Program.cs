8e4f68f2ab2b8d186f9b0ebdb98e39126d5f52cd
Game <ilja200303@gmail.com>
2020-04-21 10:07:12 +0300

    addd

--- Snake1/Program.cs
+++ Snake1/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Media;
+using System.IO;
 
 namespace Snake1
 {
@@ -11,8 +13,19 @@ namespace Snake1
     {
         static void Main(string[] args)
         {
+			SoundPlayer player = new SoundPlayer();
+			player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "music.wav";
+			player.Play();
 
-            Console.SetWindowSize(80, 25);
+			SoundPlayer player1 = new SoundPlayer();
+			player1.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "pop.wav";
+
+			SoundPlayer player2 = new SoundPlayer();
+			player2.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "died.wav";
+			//player2.Play();
+
+
+			Console.SetWindowSize(80, 25);
 
 			Walls walls = new Walls(80, 25);
 			walls.Draw();
@@ -29,12 +42,31 @@ namespace Snake1
 			{
 				if (walls.IsHit(snake) || snake.IsHitTail())
 				{
+					player2.Play();
+					try
+					{ 
+						Console.Write("Ente youe name:");
+						String nimi = (Console.ReadLine());
+						if (nimi.Length >= 3)
+						{
+							StreamWriter to_file = new StreamWriter("Answer.txt", true);
+							to_file.WriteLine(nimi);
+							to_file.Close();
+						}
+					}
+					catch(Exception)
+					{
+						Console.WriteLine("Try again pleas!");
+					}
+
 					break;
+
 				}
 				if (snake.Eat(food))
 				{
 					food = foodCreator.CreateFood();
 					food.Draw();
+					player1.Play();
 				}
 				else
 				{
